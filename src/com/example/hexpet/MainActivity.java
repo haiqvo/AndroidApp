c4aaca4dede45e0badc43d027dfdb9b36e5a1aef
Hai Vo <hai@hai-SVE15128CXS.(none)>
2014-05-29 00:09:21 -0700

    got locationListener to work

--- src/com/example/hexpet/MainActivity.java
+++ src/com/example/hexpet/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.hexpet;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
 
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -14,17 +21,34 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements
+	LocationListener{
 
+	private LocationManager locationManager;
+	private GoogleMap map;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		GoogleMap map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
         map.setMyLocationEnabled(true);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 400, 500, this);
+        
 	}
 	
+	@Override
+	protected void onStart() {
+        super.onStart();
+        // Connect the client.
+    }
 	
+	@Override
+	protected void onStop() {
+	   // Disconnecting the client invalidates it.
+	    super.onStop();
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -62,5 +86,35 @@ public class MainActivity extends ActionBarActivity {
 			return rootView;
 		}
 	}
+	
+	
+	//From here is for the locationListen
+	@Override
+	public void onLocationChanged(Location location) {
+	    LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+	    CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 15);
+	    map.animateCamera(cameraUpdate);
+	    locationManager.removeUpdates(this);
+
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
+	}
+	//locationListener ends here
 
 }
